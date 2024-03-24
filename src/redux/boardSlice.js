70be0f9125f2211b5696e1591d5d3c2fc7cb0fe9
AwaisAmin <awaisamin090@gmail.com>
2024-03-24 15:28:10 +0500

    config redux

---
+++ src/redux/boardSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+import data from "../data/data.json";
+
+const boardSlice = createSlice({
+  name: "boards",
+  initialState: data.boards,
+  reducers: {},
+});
+
+export default boardSlice;
