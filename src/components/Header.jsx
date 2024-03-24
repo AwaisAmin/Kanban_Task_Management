5fe666a47b8cdf0ff8d1d7bd4d55fadca6128371
AwaisAmin <awaisamin090@gmail.com>
2024-03-24 16:32:05 +0500

    added header dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo-mobile.svg";
 import iconDown from "../assets/icon-chevron-down.svg";
 import iconUp from "../assets/icon-chevron-up.svg";
 import ellipsis from "../assets/icon-vertical-ellipsis.svg";
+import HeaderDropDown from "./HeaderDropDown";
 
 const Header = () => {
   const [openDropdown, setOpenDropdown] = useState(false);
@@ -29,7 +30,7 @@ const Header = () => {
         </div>
 
         <div className="flex space-x-4 items-center md:space-x-6">
-          <button className="button">+ Add New Task</button>
+          <button className="hidden md:block button">+ Add New Task</button>
           <button className="button py-1 px-3 md:hidden">+</button>
           <img
             src={ellipsis}
@@ -38,6 +39,8 @@ const Header = () => {
           />
         </div>
       </header>
+
+      {openDropdown && <HeaderDropDown setOpenDropdown={setOpenDropdown} />}
     </div>
   );
 };
